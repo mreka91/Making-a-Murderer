e79d07eba42cece8fa553b63555385dffb30e413
Reka Madarasz <mreka91@gmail.com>
2020-11-04 17:13:15 +0100

    added emojis

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Solution</title>
-
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2290%22>💰</text></svg>" />
 </head>
 
 <body>
@@ -15,23 +15,20 @@
         <input type="text" name="name" id="name" required>
         <button type="submit">Submit</button>
     </form>
+    <div class="solution">
+        <?php
+        if (isset($_POST['name'])) {
 
-    <?php
-    if (isset($_POST['name'])) {
+            $name = $_POST['name'];
 
-        $name = $_POST['name'];
-
-        if ($name === 'Anders Page') {
-            echo 'YAY! You got it!';
-        } else {
-            echo 'NAY! Try again.';
+            if ($name === 'Anders Page') {
+                echo '✨YAY! You got it!✨';
+            } else {
+                echo '🙅NAY! Try again.🙅';
+            }
         }
-    }
-
-
-
-    ?>
-
+        ?>
+    </div>
 </body>
 
 </html>
